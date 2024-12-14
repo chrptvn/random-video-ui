76ffa1145e1804ceaf9508b4a7abe0cb7720869e
chrptvn <chrptvn@gmail.com>
2024-12-13 20:29:23 -0500

    Added site warning

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,13 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { VideoFormComponent } from '../video-form/video-form.component';
 import { LastSeenVideoLinkComponent } from '../last-seen-video-link/last-seen-video-link.component';
 import { RandomVideoComponent } from '../random-video/random-video.component';
 import { FooterComponent } from '../footer/footer.component';
-import {VideoLinkService} from "../../services/video-link.service";
-import {Host} from "../../models/host.model";
-import {Observable} from "rxjs";
-import {AsyncPipe, NgIf} from "@angular/common";
+import { VideoLinkService } from "../../services/video-link.service";
+import { Host } from "../../models/host.model";
+import { Observable } from "rxjs";
+import { AsyncPipe, NgIf } from "@angular/common";
+import { AgeVerificationComponent } from '../age-verification/age-verification.component';
 
 @Component({
   selector: 'app-home',
@@ -17,12 +18,22 @@ import {AsyncPipe, NgIf} from "@angular/common";
     RandomVideoComponent,
     FooterComponent,
     NgIf,
-    AsyncPipe
+    AsyncPipe,
+    AgeVerificationComponent
   ],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
   hosts$: Observable<Host[]> = this.videoLinkService.getHosts();
+  showAgeVerification = !localStorage.getItem('age-verified');
 
   constructor(private videoLinkService: VideoLinkService) {}
+
+  onAgeVerified(): void {
+    this.showAgeVerification = false;
+  }
+
+  onAgeRejected(): void {
+    // Handled in the component
+  }
 }
